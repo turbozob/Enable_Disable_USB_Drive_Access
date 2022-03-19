43822797c7d2d05eeb51b873abc1e4e65f5c3a6a
turbozob <turbozob@live.com>
2022-03-19 13:57:33 +0200

    v1.0.0.1
    update assembly name

--- Enable_Disable_USB_Drive_Access/Properties/AssemblyInfo.cs
+++ Enable_Disable_USB_Drive_Access/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Enable_Disable_USB_Drive_Access")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("USB_Drive_Access")]
+[assembly: AssemblyDescription("This software can enable or disable USB drive access")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Enable_Disable_USB_Drive_Access")]
+[assembly: AssemblyProduct("USB_Drive_Access")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
